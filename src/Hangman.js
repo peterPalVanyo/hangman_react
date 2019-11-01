760bbc859ab23214a6e3c2820e3628b7860af747
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-01 08:57:40 +0100

    three function with basic body

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,31 +1,56 @@
-import React, {Component} from 'react'
-import './Hangman.css'
-import png0 from './jpg/0.png'
-import png1 from './jpg/1.png'
-import png2 from './jpg/2.png'
-import png3 from './jpg/3.png'
-import png4 from './jpg/4.png'
-import png5 from './jpg/5.png'
-import png6 from './jpg/6.png'
+import React, { Component } from "react";
+import "./Hangman.css";
+import png0 from "./jpg/0.png";
+import png1 from "./jpg/1.png";
+import png2 from "./jpg/2.png";
+import png3 from "./jpg/3.png";
+import png4 from "./jpg/4.png";
+import png5 from "./jpg/5.png";
+import png6 from "./jpg/6.png";
 
 class Hangman extends Component {
-    static defaultProps = {
-        miss: 6,
-        images:[png0, png1, png2, png3, png4, png5, png6]
-    }
-    constructor(props) {
-        super(props)
-        this.state = {wrong:0,guessed:new Set(),word:'something'}
-    }
-    render() {
-        return (
-            <div className='Hangman'>
-                <img src={this.props.images[3]} alt='png0'/>
-                <p className='Hangman-word'>{this.state.word}</p>
-                <p className='Hangman-btns'>btns</p>
-            </div>
-        )
-    }
+  static defaultProps = {
+    chance: 6,
+    images: [png0, png1, png2, png3, png4, png5, png6]
+  };
+  constructor(props) {
+    super(props);
+    this.state = { wrong: 0, guessed: new Set(), word: "something" };
+    this.handleGuess = this.handleGuess.bind(this);
+  }
+  presentWord() {
+    return this.state.word
+      .split("")
+      .map(letter => (this.state.guessed.has(letter) ? letter : "_"));
+  }
+  handleGuess(e) {
+    const guessedLetter = e.target.value;
+    this.setState(st => ({
+      guessed: st.guessed.add(guessedLetter),
+      wrong: st.wrong + (st.word.includes(guessedLetter) ? 0 : 1)
+    }));
+  }
+  generateButtons() {
+    return "abcdefghijklmnopqrstuvwxyz".split("").map(letter => (
+      <button
+        key={letter}
+        value={letter}
+        onClick={this.handleGuess}
+        disabled={this.state.guessed.has(letter)}
+      >
+        {letter}
+      </button>
+    ));
+  }
+  render() {
+    return (
+      <div className="Hangman">
+        <img src={this.props.images[3]} alt="png0" />
+        <p className="Hangman-word">{this.presentWord()}</p>
+        <p className="Hangman-btns">{this.generateButtons()}</p>
+      </div>
+    );
+  }
 }
 
-export default Hangman
+export default Hangman;
